52dc1ab20afb282513bd14cde349f9afca22034e
michael garrison <mgarrison.sa@gmail.com>
2020-10-29 20:08:39 -0500

    project setup

---
+++ src/main/java/com/personalsite/personalsite/PersonalsiteApplication.java
@@ -0,0 +1,13 @@
+package com.personalsite.personalsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PersonalsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PersonalsiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/personalsite/personalsite/PersonalsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personalsite.personalsite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonalsiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
