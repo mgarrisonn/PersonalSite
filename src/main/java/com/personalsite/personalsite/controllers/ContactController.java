2467c369e37d12c01c98fbff891bc5181bb6d57b
michael garrison <mgarrison.sa@gmail.com>
2020-10-30 21:17:08 -0500

    created contact controller for get/post mapping for submitting contact inforomation

---
+++ src/main/java/com/personalsite/personalsite/controllers/ContactController.java
@@ -0,0 +1,39 @@
+package com.personalsite.personalsite.controllers;
+
+import com.personalsite.personalsite.models.Contact;
+import com.personalsite.personalsite.repositories.ContactRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
+public class ContactController {
+
+    private final ContactRepository contactDao;
+
+
+    public ContactController(ContactRepository contactDao) {
+        this.contactDao = contactDao;
+    }
+
+    @GetMapping("/contact")
+    public String contact(){
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String contactRequest(@ModelAttribute Contact contact){
+        Contact contacts = contactDao.save(contact);
+        contacts.setCreateDateTime(contacts.getCreateDateTime());
+        contacts.setEmail(contacts.getEmail());
+        contacts.setFirstName(contacts.getFirstName());
+        contacts.setLastName(contacts.getLastName());
+        contacts.setPhone(contacts.getPhone());
+        contacts.setNote(contacts.getNote());
+        contacts.setId(contacts.getId());
+        contactDao.save(contacts);
+        return "redirect:/";
+    }
+}
