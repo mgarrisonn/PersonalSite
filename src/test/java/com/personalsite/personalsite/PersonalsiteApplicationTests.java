1903b3033f9300831e6dff0b4c610fcf21d62955
michael garrison <mgarrison.sa@gmail.com>
2020-11-03 21:45:09 -0600

    refactored test

--- src/test/java/com/personalsite/personalsite/PersonalsiteApplicationTests.java
+++ src/test/java/com/personalsite/personalsite/PersonalsiteApplicationTests.java
@@ -1,10 +1,13 @@
 package com.personalsite.personalsite;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
+@RunWith(SpringRunner.class)
 @SpringBootTest
-class PersonalsiteApplicationTests {
+public class PersonalsiteApplicationTests {
 
     @Test
     void contextLoads() {
